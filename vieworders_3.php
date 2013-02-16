5635061be5d8ffa1b328fddbe7ac163fc5934bde
nazimov1979 <nazimov1979@ya.ru>
2013-02-16 10:39:35 +0600

    Update vieworders_3.php

--- vieworders_3.php
+++ vieworders_3.php
@@ -14,7 +14,7 @@ require('header.inc');
 
   // Создать короткие имена переменных
 
-  //$DOCUMENT_ROOT = $HTTP_SERVER_VARS['DOCUMENT_ROOT'];
+  $DOCUMENT_ROOT = $HTTP_SERVER_VARS['DOCUMENT_ROOT'];
 
 ?>
 
